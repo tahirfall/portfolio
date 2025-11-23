76778eb45dddcb0bc3f7a41df49c9520cf9ca656
Tahir FALL <falltahir20@gmail.com>
2025-11-23 19:31:24 +0000

    updating..2311-18

--- portfolio-frontend/src/app/components/navbar/navbar.component.ts
+++ portfolio-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  menuOpen: boolean = false;
 
+  constructor() {}
 }
